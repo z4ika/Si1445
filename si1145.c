502b5444e544a15b6f5a13de5546fe3e281a99ff
Koniiko <5898219+Koniiko@users.noreply.github.com>
2021-07-28 14:48:42 +0200

    Addresses pointers fixes externs reference marks in header

--- si1145.c
+++ si1145.c
@@ -1,65 +1,67 @@
 #include "si1145.h"
 
+uint8_t data = 0;
+
 // Autonomous PLS & ALS
 
 uint8_t readValue(uint8_t v)
 {
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_COMMAND, I2C_MEM_SIZE_8BIT, SI1145_PARAM_QUERY, 1, 100);
-    v = HAL_I2C_Mem_Read(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_PARAMRD, I2C_MEMADD_SIZE_8BIT, &data, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_COMMAND, I2C_MEMADD_SIZE_8BIT , SI1145_PARAM_QUERY, 1, 100);
+    v = HAL_I2C_Mem_Read(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_PARAMRD, I2C_MEMADD_SIZE_8BIT, &data, 1, 100);
     return v;
 }
 
-void si1145_startAuto(I2C_HandleTypeDef* hi2c1)
+void si1145_startAuto()
 {
     // Enable coefficients
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_UCOEFF0, I2C_MEM_SIZE_8BIT, 0x29, 1, 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_UCOEFF1, I2C_MEM_SIZE_8BIT, 0x89, 1, 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_UCOEFF2, I2C_MEM_SIZE_8BIT, 0x82, 1, 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_UCOEFF3, I2C_MEM_SIZE_8BIT, 0x00, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_UCOEFF0, I2C_MEMADD_SIZE_8BIT , (uint8_t*) 0x29, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_UCOEFF1, I2C_MEMADD_SIZE_8BIT , (uint8_t*) 0x89, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_UCOEFF2, I2C_MEMADD_SIZE_8BIT , (uint8_t*) 0x82, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_UCOEFF3, I2C_MEMADD_SIZE_8BIT , (uint8_t*) 0x00, 1, 100);
 
     // Measurement rate setting for auto mode
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_MEASRATE0, I2C_MEM_SIZE_8BIT, 0xFF, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_MEASRATE0, I2C_MEMADD_SIZE_8BIT , (uint8_t*) 0xFF, 1, 100);
 
     // readValue -> write to registry
-    readValue(SI1145_PARAM_CHLIST,SI1145_PARAM_CHLIST_ENUV | SI1145_PARAM_CHLIST_ENALSIR | SI1145_PARAM_CHLIST_ENALSVIS | SI1145_PARAM_CHLIST_ENPS1);
+    readValue(SI1145_PARAM_CHLIST | SI1145_PARAM_CHLIST_ENUV | SI1145_PARAM_CHLIST_ENALSIR | SI1145_PARAM_CHLIST_ENALSVIS | SI1145_PARAM_CHLIST_ENPS1);
 
     // Enable interrupts in between each sample
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_INTCFG, I2C_MEM_SIZE_8BIT, SI1145_REG_INTCFG_INTOE, 1, 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_INTCFG, I2C_MEM_SIZE_8BIT, SI1145_REG_IRQEN_ALSEVERYSAMPLE, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_INTCFG, I2C_MEMADD_SIZE_8BIT ,  SI1145_REG_INTCFG_INTOE, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_INTCFG, I2C_MEMADD_SIZE_8BIT ,  SI1145_REG_IRQEN_ALSEVERYSAMPLE, 1, 100);
 
     //
     // Proximity sensor should be initialised here - TODO
     //
 
-    uint8_t data;
+    uint8_t data = 0;
     data = SI1145_PSALS_AUTO;
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_COMMAND, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_COMMAND, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
 }
 
 // Reset
 
-void si1145_reset(I2C_HandleTypeDef* hi2c1)
+void si1145_reset()
 {
-    uint8_t data = 0;
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_MEASRATE0, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_MEASRATE1, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_IRQEN, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_IRQMODE1, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_IRQMODE2, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_INTCFG, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_IRQSTAT, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_MEASRATE0, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_MEASRATE1, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_IRQEN, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_IRQMODE1, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_IRQMODE2, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_INTCFG, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_IRQSTAT, I2C_MEMADD_SIZE_8BIT, &data, sizeof(data), 100);
 
     uint8_t reset = SI1145_RESET;
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_COMMAND, I2C_MEMADD_SIZE_8BIT, &reset, sizeof(reset), 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_COMMAND, I2C_MEMADD_SIZE_8BIT, &reset, sizeof(reset), 100);
 
-    HAL_I2C_Mem_Write(hi2c1, SI1145_PARAM_ADDR, SI1145_REG_HW_KEY, I2C_MEMADD_SIZE_8BIT, 0x17, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_HW_KEY, I2C_MEMADD_SIZE_8BIT, (uint8_t*) 0x17, 1, 100);
 }
 
+/*
 // Is the device ready?
 
 bool si1145_isRdy(void)
 {
-    if (HAL_I2C_IsDeviceReady(&hi2c11, SI1145_PARAM_ADDR, SI1145_REG_COMMAND, I2C_MEMADD_SIZE_8BIT, 100) != 1)
+    if (HAL_I2C_IsDeviceReady(&&hi2c1, SI1145_PARAM_ADDR, SI1145_REG_COMMAND, I2C_MEMADD_SIZE_8BIT, 100) != 1)
     {
         return false;
     }
@@ -68,32 +70,33 @@ bool si1145_isRdy(void)
         return true;
     }
 }
+*/
 
 // Read UV measurements
 
-uint16_t readUltraViolet(void)
+uint16_t readUltraViolet()
 {
-    uint16_t data;
-    HAL_I2C_Mem_Read(hi2c1, SI1145_PARAM_ADDR, 0x2C, I2C_MEMADD_SIZE_16BIT, &data, 2, 100);
+    uint8_t data = 0;
+    HAL_I2C_Mem_Read(&hi2c1, SI1145_PARAM_ADDR, 0x2C, I2C_MEMADD_SIZE_16BIT, &data, 2, 100);
     data = data / 100;
     return data;
 }
 
 // Read visible light spectrum measurements
 
-uint16_t readVisible(void)
+uint16_t readVisible()
 {
-    uint16_t data;
-    HAL_I2C_Mem_Read(hi2c1, SI1145_PARAM_ADDR, 0x22, I2C_MEMADD_SIZE_16BIT, &data, 2, 100);
+    uint8_t data = 0;
+    HAL_I2C_Mem_Read(&hi2c1, SI1145_PARAM_ADDR, 0x22, I2C_MEMADD_SIZE_16BIT, &data, 2, 100);
     return data;
 }
 
 // Read IR measurements
 
-uint16_t readInfrared(void)
+uint16_t readInfrared()
 {
-    uint16_t data;
-    HAL_I2C_Mem_Read(hi2c1, SI1145_PARAM_ADDR, 0x24, I2C_MEMADD_SIZE_16BIT, &data, 2, 100);
+    uint8_t data = 0;
+    HAL_I2C_Mem_Read(&hi2c1, SI1145_PARAM_ADDR, 0x24, I2C_MEMADD_SIZE_16BIT, &data, 2, 100);
     return data;
 }
 
@@ -103,6 +106,6 @@ uint16_t readInfrared(void)
 uint16_t readProximity(void)
 {
     uint16_t data;
-    HAL_I2C_Mem_Read(hi2c1, SI1145_PARAM_ADDR, 0x26, I2C_MEMADD_SIZE_16BIT, &data, 2, 100);
+    HAL_I2C_Mem_Read(&hi2c1, SI1145_PARAM_ADDR, 0x26, I2C_MEMADD_SIZE_16BIT, &data, 2, 100);
     return data;
 }*/
--- si1145.h
+++ si1145.h
@@ -1,32 +1,40 @@
-#include "main.h"
-#include "stm32f1xx_it.h"
+#include "i2c.h"
 
 #ifndef SI1445_SI1145_H
 #define SI1445_SI1145_H
 
-#define SI1145_PARAM_QUERY 0x80
-#define SI1145_RESET 0x01
-#define SI1145_PSALS_AUTO 0x0F
-#define SI1145_PARAM_ADDR 0x60
-#define SI1145_PARAM_CHLIST 0x01
-#define SI1145_PARAM_CHLIST_ENALSIR 0x20
-#define SI1145_PARAM_CHLIST_ENALSVIS 0x10
-#define SI1145_PARAM_CHLIST_ENPS1 0x01
-#define SI1145_REG_INTCFG 0x03
-#define SI1145_REG_INTCFG_INTOE 0x01
-#define SI1145_REG_IRQEN 0x04
-#define SI1145_REG_IRQEN_ALSEVERYSAMPLE 0x01
-#define SI1145_REG_IRQMODE1 0x05
-#define SI1145_REG_IRQMODE2 0x06
-#define SI1145_REG_HW_KEY 0x07
-#define SI1145_REG_MEASRATE0 0x08
-#define SI1145_REG_MEASRATE1 0x09
-#define SI1145_REG_UCOEFF0 0x13
-#define SI1145_REG_UCOEFF1 0x14c
-#define SI1145_REG_UCOEFF2 0x15
-#define SI1145_REG_UCOEFF3 0x16
-#define SI1145_REG_COMMAND 0x18
-#define SI1145_REG_IRQSTAT 0x21
-#define SI1145_REG_PARAMRD 0x2E
+//uint8_t SI1145_PARAM_QUERY = 0x80;
+
+#define SI1145_PARAM_QUERY 		    	(uint8_t*) 0x80
+#define SI1145_RESET 			    	(uint8_t*) 0x01
+#define SI1145_PSALS_AUTO      	     	(uint8_t*) 0x0F
+#define SI1145_PARAM_ADDR 		     	(uint8_t*) 0x60
+#define SI1145_PARAM_CHLIST 	 	 	(uint8_t*) 0x01
+#define SI1145_PARAM_CHLIST_ENALSIR  	(uint8_t*) 0x20
+#define SI1145_PARAM_CHLIST_ENALSVIS 	(uint8_t*) 0x10
+#define SI1145_PARAM_CHLIST_ENPS1    	(uint8_t*) 0x01
+#define SI1145_REG_INTCFG 			 	(uint8_t*) 0x03
+#define SI1145_REG_INTCFG_INTOE 	 	(uint8_t*) 0x01
+#define SI1145_REG_IRQEN 			 	(uint8_t*) 0x04
+#define SI1145_REG_IRQEN_ALSEVERYSAMPLE (uint8_t*) 0x01
+#define SI1145_REG_IRQMODE1 			(uint8_t*) 0x05
+#define SI1145_REG_IRQMODE2 			(uint8_t*) 0x06
+#define SI1145_REG_HW_KEY 				(uint8_t*) 0x07
+#define SI1145_REG_MEASRATE0		    (uint8_t*) 0x08
+#define SI1145_REG_MEASRATE1 			(uint8_t*) 0x09
+#define SI1145_REG_UCOEFF0 				(uint8_t*) 0x13
+#define SI1145_REG_UCOEFF1 				(uint8_t*) 0x14c
+#define SI1145_REG_UCOEFF2 				(uint8_t*) 0x15
+#define SI1145_REG_UCOEFF3 				(uint8_t*) 0x16
+#define SI1145_REG_COMMAND 				(uint8_t*) 0x18
+#define SI1145_REG_IRQSTAT 				(uint8_t*) 0x21
+#define SI1145_REG_PARAMRD 				(uint8_t*) 0x2E
+
+extern uint8_t readValue(uint8_t v);
+extern void si1145_startAuto();
+extern void si1145_reset();
+extern uint16_t readUltraViolet();
+extern uint16_t readVisible();
+extern uint16_t readInfrared();
 
 #endif
